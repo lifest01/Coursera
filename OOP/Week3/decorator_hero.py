a7479f2c7cb8e497dba1acc9ccfbf95a65d3e7a3
lifest <01stas01@gmail.com>
2021-07-13 23:54:38 +0300

    update

--- OOP/Week3/decorator_hero.py
+++ OOP/Week3/decorator_hero.py
@@ -17,7 +17,6 @@ class Hero:
             "Agility": 8,  # ловкость
             "Luck": 1  # удача
         }
-
     def get_positive_effects(self):
         return self.positive_effects.copy()
 
@@ -30,44 +29,68 @@ class Hero:
 
 class AbstractEffect(Hero, ABC):
 
-    def __init__(self,obj):
+    def __init__(self, base):
         super().__init__()
-        self.obj = obj
+        self.base = base
 
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
 
-    # def get_positive_effects(self):
-    #     self.get_positive_effects()
-    #     return positive
-    #
-    #
-    # def get_negative_effects(self):
-    #     return self.get_negative_effects()
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+    @abstractmethod
+    def get_stats(self):
+        pass
 
 
 class AbstractPositive(AbstractEffect):
-    pass
-    # def get_positive_effects(self):
-    #     return self.get_positive_effects()
-    #
-    # def get_negative_effects(self):
-    #     pass
+
+    def get_stats(self):
+        return self.base.get_stats.copy()
 
 
 class AbstractNegative(AbstractEffect):
-    pass
-    # def get_negative_effects(self):
-    #     return self.get_negative_effects()
-    #
-    # def get_positive_effects(self):
-    #     pass
+
+    def get_stats(self):
+        return self.base.get_stats.copy()
 
 
 class Berserk(AbstractPositive):
 
-    def get_positive_effects(self):
-        return self.positive_effects.append('Berserk')
+    def __init__(self,base):
+        super().__init__(base)
+        # self.base.positive_effects = self.positive_effects
+        # self.stats = self.base.get_stats
+        self.positive_effects.append('Berserk')
+        # self.stats['Strength'] += 7
+        # self.stats['Endurance'] += 7
+        # self.stats['Agility'] += 7
+        # self.stats['Luck'] += 7
+        # self.stats['Perception'] -= 7
+        # self.stats['Charisma'] -= 7
+        # self.stats['Intelligence'] -= 3
+        # self.stats['HP'] += 50
+# hero = Hero()
+# brs = Berserk(hero)
+# "HP": 128,  # health points
+#             "MP": 42,  # magic points,
+#             "SP": 100,  # skill points
+#             "Strength": 15,  # сила
+#             "Perception": 4,  # восприятие
+#             "Endurance": 8,  # выносливость
+#             "Charisma": 2,  # харизма
+#             "Intelligence": 3,  # интеллект
+#             "Agility": 8,  # ловкость
+#             "Luck": 1  # удача
 
 class Blessing(AbstractPositive):
+
+    def __init__(self,base):
+        super().__init__(base)
+        # self.base.positive_effects = self.positive_effects
+        # self.stats = self.base.get_stats
+        self.positive_effects.append('Blessing')
 
     def get_positive_effects(self):
         self.positive_effects.append('Blessing')
@@ -84,8 +107,8 @@ class Curse(AbstractNegative):
 class EvilEye(AbstractNegative):
     pass
 
-
-# hero = Hero()
+hero = Hero()
 # print(hero.get_stats())
-# brs = Berserk(hero)
+brs = Berserk(hero)
 # print(brs.get_positive_effects())
+bls = Blessing(brs)
